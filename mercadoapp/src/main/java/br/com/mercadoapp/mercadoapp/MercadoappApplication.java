9ce2b5c446fd03815b8d642f1146af770e459239
Camilo LR <autumndrums@hotmail.com>
2025-06-24 16:58:48 -0300

    Projeto MercadoApp criado

---
+++ mercadoapp/src/main/java/br/com/mercadoapp/mercadoapp/MercadoappApplication.java
@@ -0,0 +1,13 @@
+package br.com.mercadoapp.mercadoapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MercadoappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MercadoappApplication.class, args);
+	}
+
+}
---
+++ mercadoapp/src/test/java/br/com/mercadoapp/mercadoapp/MercadoappApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadoapp.mercadoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
