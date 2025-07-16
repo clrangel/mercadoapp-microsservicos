71bb708709fb6f1b1a7739610f45597f4c10e6c5
Camilo LR <autumndrums@hotmail.com>
2025-07-16 14:12:53 -0300

    Microsserviço de Pedidos

---
+++ ms-pedidos/src/test/java/br/com/mercadoapp/ms_pedidos/MsPedidosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.mercadoapp.ms_pedidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPedidosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
