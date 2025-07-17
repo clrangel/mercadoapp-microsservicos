403c649d1459b5d1798d777a088c2c87eb96552c
Camilo LR <autumndrums@hotmail.com>
2025-07-17 14:58:45 -0300

    Microsserviço de Pedidos: Migration para criar a tabela de Pedido e ItemPedido

---
+++ ms-pedidos/src/main/resources/db/migration/V1__create_table_pedido_itempedido.sql
@@ -0,0 +1,16 @@
+CREATE TABLE pedido (
+    id CHAR(36) PRIMARY KEY,
+    date_moment TIMESTAMP NOT NULL,
+    valor_total DECIMAL(19, 2) NOT NULL,
+    status VARCHAR(255) NOT NULL,
+    usuario_id BIGINT NOT NULL
+);
+
+CREATE TABLE item_pedido (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    quantidade INTEGER NOT NULL,
+    valor_unitario DECIMAL(19, 2) NOT NULL,
+    pedido_id CHAR(36) NOT NULL,
+    produto_id BIGINT NOT NULL,
+    CONSTRAINT fk_itempedido_pedido FOREIGN KEY (pedido_id) REFERENCES pedido(id)
+);
