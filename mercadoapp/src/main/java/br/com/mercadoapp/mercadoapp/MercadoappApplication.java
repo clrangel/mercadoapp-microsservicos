2b71609462e2b192b561e0c2d433e95feb4bb050
Camilo LR <autumndrums@hotmail.com>
2025-07-28 15:29:56 -0300

    Microsserviço MercadoApp: Configuração do Eureka Client para conexão com o Eureka Server

--- mercadoapp/src/main/java/br/com/mercadoapp/mercadoapp/MercadoappApplication.java
+++ mercadoapp/src/main/java/br/com/mercadoapp/mercadoapp/MercadoappApplication.java
@@ -2,9 +2,11 @@ package br.com.mercadoapp.mercadoapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.data.web.config.EnableSpringDataWebSupport;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MercadoappApplication {
 
 	public static void main(String[] args) {
