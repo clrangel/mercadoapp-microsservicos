4e96967a4db2bc6ed15ea5d981d7a2e84c0987d7
Camilo LR <autumndrums@hotmail.com>
2025-08-04 16:07:27 -0300

    Configuração do Eureka Client e habilitação do Feign Client no microsserviço de pagamentos

--- ms-pagamentos/src/main/java/br/com/mercadoapp/ms/pagamentos/MsPagamentosApplication.java
+++ ms-pagamentos/src/main/java/br/com/mercadoapp/ms/pagamentos/MsPagamentosApplication.java
@@ -2,8 +2,12 @@ package br.com.mercadoapp.ms.pagamentos;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class MsPagamentosApplication {
 
 	public static void main(String[] args) {
