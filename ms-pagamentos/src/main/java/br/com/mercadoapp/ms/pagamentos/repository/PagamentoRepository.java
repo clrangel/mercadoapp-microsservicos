555346452e545037aea3abc4a3991a3cc98448c6
Camilo LR <autumndrums@hotmail.com>
2025-08-04 17:47:55 -0300

    Microsserviço Pagamento: Atualização no PagamentoRepository

--- ms-pagamentos/src/main/java/br/com/mercadoapp/ms/pagamentos/repository/PagamentoRepository.java
+++ ms-pagamentos/src/main/java/br/com/mercadoapp/ms/pagamentos/repository/PagamentoRepository.java
@@ -1,7 +1,11 @@
-package br.com.mercadoapp.ms.pagamentos;
+package br.com.mercadoapp.ms.pagamentos.repository;
 
 import br.com.mercadoapp.ms.pagamentos.model.Pagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+import java.util.UUID;
+
 public interface PagamentoRepository extends JpaRepository<Pagamento, Long> {
+    Optional<Pagamento> findByPedidoId(UUID pedidoId);
 }
