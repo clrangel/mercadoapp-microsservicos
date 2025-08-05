1b77ef896a9f3a83a93a9c147122e986d650338d
Camilo LR <autumndrums@hotmail.com>
2025-08-05 15:24:28 -0300

    Microsserviço Pagamento: PedidoResponseDto e enum Status

---
+++ ms-pagamentos/src/main/java/br/com/mercadoapp/ms/pagamentos/client/dto/PedidoResponseDto.java
@@ -0,0 +1,25 @@
+package br.com.mercadoapp.ms.pagamentos.client.dto;
+
+import br.com.mercadoapp.ms.pagamentos.client.enums.Status;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+import java.util.List;
+import java.util.UUID;
+
+public record PedidoResponseDto(
+        UUID id,
+        Instant dateMoment,
+        BigDecimal valorTotal,
+        Status status,
+        Long usuarioId,
+        List<ItemPedidoDto> itens
+) {}
+
+record ItemPedidoDto(
+        Long id,
+        UUID produtoId,
+        String nomeProduto,
+        Integer quantidade,
+        BigDecimal valorUnitario
+) {}
