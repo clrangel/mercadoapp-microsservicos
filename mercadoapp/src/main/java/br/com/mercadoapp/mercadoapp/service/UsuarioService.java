45095352843da8a03b1ffaf1d3764315e320398d
Camilo LR <autumndrums@hotmail.com>
2025-09-30 16:19:50 -0300

    Microsserviço MercadoApp: UsuarioService - Implementado o método enviarMensagem e atualização do UsuarioRepository para buscar um usuário pelo cpf.

--- mercadoapp/src/main/java/br/com/mercadoapp/mercadoapp/service/UsuarioService.java
+++ mercadoapp/src/main/java/br/com/mercadoapp/mercadoapp/service/UsuarioService.java
@@ -1,10 +1,13 @@
 package br.com.mercadoapp.mercadoapp.service;
 
+import br.com.mercadoapp.mercadoapp.dto.EmailDto;
 import br.com.mercadoapp.mercadoapp.dto.UsuarioDto;
 import br.com.mercadoapp.mercadoapp.model.Usuario;
 import br.com.mercadoapp.mercadoapp.repository.UsuarioRepository;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,6 +18,9 @@ public class UsuarioService {
 
     @Autowired
     private UsuarioRepository repository;
+
+    @Autowired
+    private JavaMailSender sender;
 
     public List<Usuario> obterTodos(){
         return repository.findAll();
@@ -53,4 +59,24 @@ public class UsuarioService {
         entityUsuario.setEmail(obj.getEmail());
         entityUsuario.setTelefone(obj.getTelefone());
     }
+
+    public void enviarMensagem(EmailDto mensagem) {
+        Optional<Usuario> usuario = repository.findByCpf(mensagem.cpf());
+
+        if (usuario.isPresent()) {
+            SimpleMailMessage enviaMensagem = new SimpleMailMessage();
+
+            enviaMensagem.setFrom("SEU-EMAIL@gmail.com"); // Quem envia o e-mail
+            enviaMensagem.setTo(usuario.get().getEmail()); // Para quem será enviado. Pega o e-mail do usuário encontrado no banco.
+            enviaMensagem.setSubject("Status do pedido " + mensagem.pedidoId()); // Assunto
+            enviaMensagem.setText("O pedido está: " + mensagem.status()); // Corpo da mensagem mostrando o status do pedido.
+            try {
+                sender.send(enviaMensagem);
+                System.out.println("Mensagem enviada com sucesso!");
+            } catch (Exception e) {
+                System.out.println("Erro ao enviar mensagem!");
+            }
+        }
+    }
+
 }
